cd676f9e3c81ada81f8c508533b98fd61e9f7629
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-06-20 14:03:02 +0300

    Tabbed Components

--- script.js
+++ script.js
@@ -66,3 +66,24 @@ document.querySelector('.nav__links').addEventListener('click', function(e)
         document.querySelector(id).scrollIntoView({behavior: 'smooth'});
     };
 })
+
+//Tabbed Components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e)
+{
+    const clicked = e.target.closest('.operations__tab');
+    console.log(clicked);
+    console.log(clicked.dataset.tab);
+
+    tabs.forEach(t => t.classList.remove('operations__tab--active'));
+    tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+    clicked.classList.add('operations__tab--active');
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+
+
+
+})
